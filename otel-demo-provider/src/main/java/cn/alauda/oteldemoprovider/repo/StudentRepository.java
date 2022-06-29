91c51bd5d2f6d079b2864bb19f3d5dda543874ae
HaoTu <haotu@alauda.io>
2022-06-29 16:39:38 +0800

    Revert "update"
    This reverts commit ea5e6ef069da2cbfb68c2feee6429a0c04472cf6.

--- otel-demo-provider/src/main/java/cn/alauda/oteldemoprovider/repo/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package cn.alauda.oteldemoprovider.repo;
-
-import cn.alauda.oteldemoprovider.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Integer> {
-    Student findById(int id);
-    Student findByName(String name);
-}
